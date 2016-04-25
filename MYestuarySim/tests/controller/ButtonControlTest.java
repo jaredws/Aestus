0e6ddb23ef1d495a4bdd48fec0eeba7cfdc2c993
Steven <steven.sell1@gmail.com>
2016-04-25 14:24:13 -0400

    Started Adding Tests

---
+++ MYestuarySim/tests/controller/ButtonControlTest.java
@@ -0,0 +1,30 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ButtonControlTest {
+
+	static ButtonControl b;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		b = new ButtonControl();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		b = null;
+	}
+
+	@Test
+	public void getButtonsTest() {
+		assertEquals("Y of first button should be 500", b.getButtons().get(0).getY(),500);
+		assertEquals("Y of second button should be 625", b.getButtons().get(1).getY(),625);
+		
+	}
+
+}
---
+++ MYestuarySim/tests/controller/CrabControlTest.java
@@ -0,0 +1,30 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CrabControlTest {
+
+	static CrabControl c;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		c = new CrabControl();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		c = null;
+	}
+
+	@Test
+	public void addCrabTest() {
+		c.addCrab(3, 2);
+		assertEquals("Crab at first position should have an X of 3",c.getCrabs().get(0).getX(),3);
+		assertEquals("Crab at first position should have a Y of 2",c.getCrabs().get(0).getY(),2);
+	}
+
+}
