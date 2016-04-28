ac9d7334c537131a1c0ef58b22f00ace0cf03ba7
Steven <steven.sell1@gmail.com>
2016-04-27 21:17:46 -0400

    Added Menu Button

--- MYestuarySim/src/controller/ButtonControl.java
+++ MYestuarySim/src/controller/ButtonControl.java
@@ -12,7 +12,7 @@ public class ButtonControl {
 	public ButtonControl(int h,int w){
 		Button trash = new Button(0, 500, 0);
 		Button bucket = new Button(0, 625, 1);
-		Button menu = new Button(w-100,0,2);
+		Button menu = new Button(w-160,0,2);
 		//Will need to add more buttons later
 		Button[] bns ={trash,bucket,menu};	
 		buttons = new ArrayList<Button>();
--- MYestuarySim/src/view/ButtonView.java
+++ MYestuarySim/src/view/ButtonView.java
@@ -17,7 +17,7 @@ public class ButtonView{
 	public ButtonView(){
 		images = new ArrayList<BufferedImage>();
 		BufferedImage image;
-		String[] fileNames = {"trashB", "bucket", "bucket"};
+		String[] fileNames = {"trashB", "bucket", "menuSmall"};
 		for(String name: fileNames){
 			try {    
 				image = ImageIO.read(new File("./img/"+name+".png"));
