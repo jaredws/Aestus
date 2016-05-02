c74f840327aab86f4c08e7890f6d29f1b9e6cec8
jared <jared@wifi-roaming-128-4-102-34.host.udel.edu>
2016-05-02 12:01:09 -0400

    creating Game2

---
+++ MYestuarySim/src/controller/Game2.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class Game2 {
+
+	//implementing the combined ViewAll and ControlObjects classes
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
