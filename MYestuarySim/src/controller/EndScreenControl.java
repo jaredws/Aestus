9347a10da9df9e4c99129e4f21a30ff60c55abc3
Steven <steven.sell1@gmail.com>
2016-05-05 13:18:47 -0400

    Working On Ending Screen

--- MYestuarySim/src/controller/EndScreenControl.java
+++ MYestuarySim/src/controller/EndScreenControl.java
@@ -5,24 +5,17 @@ import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
-import javax.swing.JButton;
+import javax.swing.JPanel;
 
-public class EndScreenControl extends JButton {
-
-	//Implament listener on the frame??? -JS
+public class EndScreenControl extends JPanel {
 
 	private static final long serialVersionUID = 9042772411806531339L;
 	public int clickx;
 	public int clicky;
 
-
 	public EndScreenControl(){
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setSize((int) screenSize.getWidth(), (int)screenSize.getHeight());
-	    setBorderPainted(false);
-	    setFocusPainted(false);
-	    setContentAreaFilled(false);
-	
 	
 	addMouseListener(new MouseAdapter(){
     	//If mouse button is pressed
@@ -42,30 +35,4 @@ public class EndScreenControl extends JButton {
         }
     });
 	}
-	public void checkPos(CrabControl c){
-		//The trash can deletes if the crab is 'grabbed' and you drag it over the can
-		//do we want this? or should we wait for release
-		
-//		if(!grabbing){
-//			for(int i = 0; i < c.crabs.size(); i++){
-//				if(((x-c.crabs.get(i).sizeX/2) < c.crabs.get(i).getX()) && (c.crabs.get(i).getX() < (x+c.crabs.get(i).sizeX/2))){
-//					if(((y-c.crabs.get(i).sizeY/2) < c.crabs.get(i).getY()) && (c.crabs.get(i).getY() < (y+c.crabs.get(i).sizeY/2))){
-//						grabbed = c.crabs.get(i);
-//						grabbing = true;
-//						j = i;
-//						break;
-//						}
-//					}
-//			}
-//		}
-//		if((j > -1 && j < c.crabs.size()) && c.crabs.get(j).equals(grabbed)){
-//			c.crabs.get(j).setX(x);
-//			c.crabs.get(j).setY(y);	
-//		}
-//	}
-}
-	
-
-
-
 }
