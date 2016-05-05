b196636f01995a37c771ea5cff7057999752ab94
Steven <steven.sell1@gmail.com>
2016-05-04 22:05:35 -0400

    Figured Out How To Test View Classes

--- MYestuarySim/tests/view/CrabViewTest.java
+++ MYestuarySim/tests/view/CrabViewTest.java
@@ -1,13 +1,16 @@
 package view;
 
-import static org.junit.Assert.*;
+/**
+ * @author Steven
+ */
 
-import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
-
+import java.util.Random;
 import javax.imageio.ImageIO;
-
+import static org.junit.Assert.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,26 +18,46 @@ import org.junit.Test;
 public class CrabViewTest {
 	
 	static CrabView cv;
-	static Image image;
+	static Random rand;
+	static BufferedImage cvi;
+	static BufferedImage img;
+	static Dimension screenSize;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		cv = new CrabView();
+		cvi = Helper.toBufferedImage(cv.getImage(0));
+		rand = new Random();
+		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		try { 
-			image = ImageIO.read(new File("./img/Crab Front.png"));
-       } catch (IOException ex) {
-    	   System.out.println("Crab Image read error");
-       }
+			img = ImageIO.read(new File("./img/Crab Front.png"));
+        } catch (IOException ex) {
+    	   System.out.println("Image read error");
+        }
+		img = Helper.toBufferedImage(img.getScaledInstance((int)screenSize.getWidth()/(38+12), -1,1));
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		cv = null;
+		cvi = null;
+		img = null;
 	}
 
 	@Test
-	public void test() {
-		assertTrue(cv.getImage(0).hashCode()==image.hashCode());
+	public void imageDimensionTest() {
+		assertEquals("The height of each image should be equal",cvi.getHeight(null),img.getHeight(null));
+		assertEquals("The width of each image should be equal",cvi.getWidth(null),img.getWidth(null));
 	}
-
+	
+	@Test
+	public void imagePixelComparisonTest() {
+		int w = cvi.getHeight(null);
+		int h = cvi.getWidth(null);
+		for(int i=0;i<100;i++) {
+			int rx = rand.nextInt(w);
+			int ry = rand.nextInt(h);
+			assertEquals("The integer pixels should be equal",img.getRGB(rx, ry),cvi.getRGB(rx, ry));
+		}
+	}
 }
