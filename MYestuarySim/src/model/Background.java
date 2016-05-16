1ad7234d123271ce2e318d975c894dd21b69c513
akarpz <adame@128.4.211.236>
2016-05-16 17:37:18 -0400

    background doc

--- MYestuarySim/src/model/Background.java
+++ MYestuarySim/src/model/Background.java
@@ -1,15 +1,35 @@
 package model;
 
-
+/**
+ * The Class Background.
+ * Stores the size of the background and handles estuary health checks
+ * @author Team 0
+ */
 public class Background {
+	
+	/** The size x. */
 	private int sizeX;
+	
+	/** The size y. */
 	private int sizeY;
 	
+	/**
+	 * Instantiates a new background.
+	 *
+	 * @param x the width
+	 * @param y the height
+	 */
 	public Background(int x, int y) {
 		this.sizeX = x;
 		this.sizeY = y;
 	}
 	
+	/**
+	 * Check.
+	 *
+	 * @param health the given health of the estuary
+	 * @return the int representing the health rank of the estuary
+	 */
 	public int check(int health){
 		if(health < 15){
 			return 2;
@@ -20,10 +40,20 @@ public class Background {
 		
 	}
 	
+	/**
+	 * Gets the x.
+	 *
+	 * @return the x
+	 */
 	public int getX() {
 		return sizeX;
 	}
 	
+	/**
+	 * Gets the y.
+	 *
+	 * @return the y
+	 */
 	public int getY() {
 		return sizeY;
 	}
