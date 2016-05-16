b1965d8bdec1b57c7f9df4c575a77de6e783db73
Steven <steven.sell1@gmail.com>
2016-05-16 14:03:20 -0400

    Tests

---
+++ MYestuarySim/tests/controller/GameTest.java
@@ -0,0 +1,27 @@
+package controller;
+
+import static org.junit.Assert.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Steven Sell
+ * @Test Tests all functions of Game
+ */
+
+public class GameTest {
+
+	static Game g;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		//g = new Game();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		//g = null;
+	}
+
+}
