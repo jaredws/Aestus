da8650dad0eda62e1a002da4da6caa2c5cef259b
akarpz <adame@128.4.211.236>
2016-05-16 19:53:34 -0400

    health docs

--- MYestuarySim/src/model/Health.java
+++ MYestuarySim/src/model/Health.java
@@ -2,26 +2,41 @@ package model;
 
 import java.awt.Dimension;
 
+
 /**
- * 
- * @author Steven
- *
+ * The Class Health. Keeps track of the health of the estuary in game.
+ * @author Team 0
  */
 public class Health {
-	//With this outline, there's no difference between a crab and anyhting else. 
-	//We'll have to change the name of this class to Interactable Object or something
-	//Add a name and just name the different items
 	
+	/** The x location . */
 	private int x;
+	
+	/** The y location. */
 	private int y;
-	//The sizes may need to be a ratio to the screen size. -JS
-	//temporary public variables
+	
+	/** The size x. */
 	public static int sizeX;
+	
+	/** The size y. */
 	public static int sizeY;
+	
+	/** The move counter. */
 	public int move;
+	
+	/** The X dir. */
 	public int XDir;
+	
+	/** The Y dir. */
 	public int YDir;
 	
+	/**
+	 * Instantiates a new health with default values
+	 *
+	 * @param x the x location for spawn
+	 * @param y the y location for spawn
+	 * @param screenSize the screen size
+	 */
 	public Health(int x, int y,Dimension screenSize){
 		move = 0;
 		this.x = x;
@@ -32,34 +47,74 @@ public class Health {
 		YDir = 0;
     }
 	
+	/**
+	 * Gets the x.
+	 *
+	 * @return the x
+	 */
 	public int getX() {
 		return x;
 	}
 	
+	/**
+	 * Sets the x.
+	 *
+	 * @param x the new x
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
+	/**
+	 * Gets the y.
+	 *
+	 * @return the y
+	 */
 	public int getY() {
 		return y;
 	}
 	
+	/**
+	 * Sets the y.
+	 *
+	 * @param y the new y
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 	
+	/**
+	 * Gets the size x.
+	 *
+	 * @return the size x
+	 */
 	public int getSizeX() {
 		return Health.sizeX;
 	}
 	
+	/**
+	 * Gets the size y.
+	 *
+	 * @return the size y
+	 */
 	public int getSizeY() {
 		return Health.sizeY;
 	}
 	
+	/**
+	 * Sets the size y.
+	 *
+	 * @param y the new size y
+	 */
 	public void setSizeY(int y) {
 		Health.sizeY = y;
 	}
 	
+	/**
+	 * Sets the size x.
+	 *
+	 * @param x the new size x
+	 */
 	public void setSizeX(int x) {
 		Health.sizeX = x;
 	}
