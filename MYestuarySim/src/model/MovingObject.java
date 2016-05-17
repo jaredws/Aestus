df21ba75089418d973aa07567ae0fc9294892e52
akarpz <adame@128.4.211.236>
2016-05-16 21:08:08 -0400

    movingobject docs

--- MYestuarySim/src/model/MovingObject.java
+++ MYestuarySim/src/model/MovingObject.java
@@ -1,14 +1,14 @@
 package model;
 
 /**
- * 
- * @author Jared
- * 
- * We want each class to move like so
- *
+ * The Class MovingObject. Parent class to all objects that move on their own
+ * @author Team 0
  */
 public abstract class MovingObject extends Grabbable {
 	
+	/**
+	 * Move the object
+	 */
 	public void move(){
 		
 	}
