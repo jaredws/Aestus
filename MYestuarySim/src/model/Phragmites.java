234cbc01b9f668b10a764068723c80794ab5f087
akarpz <adame@128.4.211.236>
2016-05-16 21:26:32 -0400

    phragmites doc

--- MYestuarySim/src/model/Phragmites.java
+++ MYestuarySim/src/model/Phragmites.java
@@ -3,23 +3,36 @@ package model;
 import java.awt.Dimension;
 
 /**
- * 
- * @author Steven
- *
+ * The Class Phragmites, contains the location, size, growth state.
+ * Handled by the Phragmites handler and ScreenControl.
+ * @author Team 0
  */
 public class Phragmites extends Grabbable {
-	//With this outline, there's no difference between a crab and anyhting else. 
-	//We'll have to change the name of this class to Interactable Object or something
-	//Add a name and just name the different items
+
 	
+	/** The x location. */
 	private int x;
+	
+	/** The y location. */
 	private int y;
-	//The sizes may need to be a ratio to the screen size. -JS
-	//temporary public variables
+	
+	/** The size x. */
 	public static int sizeX;
+	
+	/** The size y. */
 	public static int sizeY;
+	
+	/** The life(growth) state. */
 	private int life;
 	
+	/**
+	 * Constructor 
+	 * Instantiates a new phragmites with default values
+	 *
+	 * @param x the x location for spawn
+	 * @param y the y location for spawn
+	 * @param screenSize the screen size
+	 */
 	public Phragmites(int x, int y, Dimension screenSize){
 		life = 0;
 		this.x = x;
@@ -30,40 +43,90 @@ public class Phragmites extends Grabbable {
 		
     }
 	
+	/**
+	 * Gets the x.
+	 *
+	 * @return the x
+	 */
 	public int getX() {
 		return x;
 	}
 	
+	/**
+	 * Sets the x.
+	 *
+	 * @param x the new x
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
+	/**
+	 * Gets the y.
+	 *
+	 * @return the y
+	 */
 	public int getY() {
 		return y;
 	}
 	
+	/**
+	 * Sets the y.
+	 *
+	 * @param y the new y
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 	
+	/**
+	 * Gets the size x.
+	 *
+	 * @return the size x
+	 */
 	public int getSizeX() {
 		return Phragmites.sizeX;
 	}
 	
+	/**
+	 * Gets the size y.
+	 *
+	 * @return the size y
+	 */
 	public int getSizeY() {
 		return Phragmites.sizeY;
 	}
 	
+	/**
+	 * Sets the size y.
+	 *
+	 * @param y the new size y
+	 */
 	public void setSizeY(int y) {
 		Phragmites.sizeY = y;
 	}
 	
+	/**
+	 * Sets the size x.
+	 *
+	 * @param x the new size x
+	 */
 	public void setSizeX(int x) {
 		Phragmites.sizeX = x;
 	}
+	
+	/**
+	 * Increases the growth state.
+	 */
 	public void live(){
 		life++;
 	}
+	
+	/**
+	 * Gets the life.
+	 *
+	 * @return the life
+	 */
 	public int getLife() {
 		return this.life;
 	}
